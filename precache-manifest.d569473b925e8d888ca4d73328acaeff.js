db06bd9ded12d4776d3b9fcf2e4bd7c73f184e80
Deepak <dd43028@gmail.com>
2020-04-27 04:43:28 +0530

    Updates

---
+++ precache-manifest.d569473b925e8d888ca4d73328acaeff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913b512de5cb1bcf498648d9373fac20",
+    "url": "/shoppingcarjw/index.html"
+  },
+  {
+    "revision": "a19a8044bc1511249e47",
+    "url": "/shoppingcarjw/static/css/main.c02cf3d3.chunk.css"
+  },
+  {
+    "revision": "bec905bda652f6c57da8",
+    "url": "/shoppingcarjw/static/js/2.c4e2b8f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoppingcarjw/static/js/2.c4e2b8f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a19a8044bc1511249e47",
+    "url": "/shoppingcarjw/static/js/main.084a255a.chunk.js"
+  },
+  {
+    "revision": "de091d79790a6bd4bc73",
+    "url": "/shoppingcarjw/static/js/runtime-main.799765d4.js"
+  },
+  {
+    "revision": "598576227d7a280c0ee687d17f64d132",
+    "url": "/shoppingcarjw/static/media/star.59857622.svg"
+  }
+]);
